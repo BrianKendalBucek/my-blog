ce33fed93d3413248c32383ca6fc5ba13064c167
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-11-18 15:11:14 -0800

    added design to bloglist and blogpost

--- src/components/BlogPost/BlogPost.tsx
+++ src/components/BlogPost/BlogPost.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getArticles, Article } from "../../api/strapiAPI";
+import "./BlogPost.css"; // Adjust the path as necessary
 
 const BlogPost: React.FC = () => {
   const { id } = useParams<{ id: string }>();
