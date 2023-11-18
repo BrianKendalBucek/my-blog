2b0621490c70a1e65f2cdc9af12f5ee258470824
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-11-18 15:56:23 -0800

    minor adjustments

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import './Navbar.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
-    return (
-        <nav>
-            <h1>My Blog</h1>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <Link to="/">
+        <h1>My Blog</h1>
+      </Link>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
